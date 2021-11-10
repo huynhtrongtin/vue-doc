ac8368f32356061d8f072fc6232ffe315332e91b
Tin Huynh <tinhuynh@bocasay.com>
2021-11-10 21:39:30 +0700

    deploy

---
+++ precache-manifest.0e9cb185127855d8eca7ba3e39896378.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = [
+  {
+    "revision": "15b5fe55ca2f0b054d62",
+    "url": "/css/app.b5408097.css"
+  },
+  {
+    "revision": "15b5fe55ca2f0b054d62",
+    "url": "/js/app.9946acde.js"
+  },
+  {
+    "revision": "a48e3fd65ed384cee38c",
+    "url": "/css/auth.aef6c3e1.css"
+  },
+  {
+    "revision": "a48e3fd65ed384cee38c",
+    "url": "/js/auth.7c5c166b.js"
+  },
+  {
+    "revision": "646511f0314765a6c5c1",
+    "url": "/css/components.826301c5.css"
+  },
+  {
+    "revision": "646511f0314765a6c5c1",
+    "url": "/js/components.b9906bac.js"
+  },
+  {
+    "revision": "8815198ae15aedae387f",
+    "url": "/js/dashboard.75f43074.js"
+  },
+  {
+    "revision": "e03e5e31d8d19777c331",
+    "url": "/css/maps.bad9c1f4.css"
+  },
+  {
+    "revision": "e03e5e31d8d19777c331",
+    "url": "/js/maps.fcecef39.js"
+  },
+  {
+    "revision": "4687aa7252dc6946de06",
+    "url": "/css/password.c6a956fd.css"
+  },
+  {
+    "revision": "4687aa7252dc6946de06",
+    "url": "/js/password.93ee78ac.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/fonts/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/fonts/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/fonts/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/fonts/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "2050f45bfd2b0920fc1faf906a21450f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "702fb8c567cd92b4f3c242abc84a8b56",
+    "url": "/img/AU.png"
+  },
+  {
+    "revision": "f8624a716c4efc245ad1550c657b5f98",
+    "url": "/img/apple-icon.png"
+  },
+  {
+    "revision": "ce0474dc19b6bb2dcfb27a489754a859",
+    "url": "/img/balmain.jpg"
+  },
+  {
+    "revision": "c8b3878b0087f2b6118106a96ee17891",
+    "url": "/img/bg13.jpg"
+  },
+  {
+    "revision": "98b6b57a2e3ee1baf1b9ac76f9a04e09",
+    "url": "/img/bg1.jpg"
+  },
+  {
+    "revision": "f5ccf1a207d9082cc6b6e0769b5c7efa",
+    "url": "/img/bg15.jpg"
+  },
+  {
+    "revision": "0a929a3f8a55e7a094af71b3b22033af",
+    "url": "/img/BR.png"
+  },
+  {
+    "revision": "53e627ca2acad7eee63a70e27c7792d7",
+    "url": "/img/bg14.jpg"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/img/default-avatar.png"
+  },
+  {
+    "revision": "f77d0df953adcfedc2cab68651a1d01b",
+    "url": "/img/bg3.jpg"
+  },
+  {
+    "revision": "64ababf7de758dcee0358c2c4d93a371",
+    "url": "/img/DE.png"
+  },
+  {
+    "revision": "a30e21b28cb8bfddda9d33d5a5e4d28b",
+    "url": "/img/emilyz.jpg"
+  },
+  {
+    "revision": "7b8a6099ce9bdadcf1bcfde26a139275",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "b0d54b4089ce5e24c82574109f346289",
+    "url": "/img/GB.png"
+  },
+  {
+    "revision": "f68d9108445df3450b1eed7ae0256fc1",
+    "url": "/img/bg5.jpg"
+  },
+  {
+    "revision": "501ba29ef8fa01dc0d2a224508ac59d5",
+    "url": "/img/icon-vue.png"
+  },
+  {
+    "revision": "cfd464b36416dab7fcdcc09e009a3819",
+    "url": "/img/bg16.jpg"
+  },
+  {
+    "revision": "49458d8716dcfc74c2e95064dbb125d2",
+    "url": "/img/header.jpg"
+  },
+  {
+    "revision": "16f109f847c0133b454f5598e55827ce",
+    "url": "/img/image_placeholder.jpg"
+  },
+  {
+    "revision": "8c8f7d2b790a7c7f399375155c3cffbe",
+    "url": "/img/james.jpg"
+  },
+  {
+    "revision": "a6178f443133606fe19006604d0cafb5",
+    "url": "/img/loading.gif"
+  },
+  {
+    "revision": "4ec805b26cd469c628ed056227ff3df1",
+    "url": "/img/mike.jpg"
+  },
+  {
+    "revision": "b43051810a4f05db4a1d2314f64aa0cf",
+    "url": "/img/now-logo.png"
+  },
+  {
+    "revision": "9a85752042ae270b88114895b82da2c1",
+    "url": "/img/pdfimg.png"
+  },
+  {
+    "revision": "54a7dcaf03d55cc8050e81d4569bd05c",
+    "url": "/img/placeholder.jpg"
+  },
+  {
+    "revision": "8abc72640ffcac89f657378be61c72c7",
+    "url": "/img/prada.jpg"
+  },
+  {
+    "revision": "54c7e270d82bafb7a5ae359aa1d318a3",
+    "url": "/img/RO.png"
+  },
+  {
+    "revision": "7465ad2c65edd67cbee2809547bfe4df",
+    "url": "/img/saint-laurent.jpg"
+  },
+  {
+    "revision": "2f88ea36fc94779a8f2e65c2ee8ccc76",
+    "url": "/img/US.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
